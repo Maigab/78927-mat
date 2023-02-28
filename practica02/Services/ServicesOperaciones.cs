5b8d5768ffc91a2b9b93d3321d4d99287b67c616
Maigab <99707834+Maigab@users.noreply.github.com>
2023-02-28 12:55:33 -0600

    Update ServicesOperaciones.cs

--- practica02/Services/ServicesOperaciones.cs
+++ practica02/Services/ServicesOperaciones.cs
@@ -4,15 +4,15 @@ using WSDL.mensajes;
 namespace WSDL.operaciones{
     public class Operaciones : Mensajes{
         
-        List<string> Saludo = new List<string>();
+        List<string> saludo = new List<string>();
         
         public string Saludar(string nombre){
             string msj = nombre;
-            Saludo.add(nombre);
+            saludo.add(nombre);
             return msj;
         }
         public string Mostrar(int id){
-            return Saludo[id-1];
+            return saludo[id-1];
         }
     }
 }
